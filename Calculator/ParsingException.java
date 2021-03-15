97ce0f52cfc5f8dbdc33a02474fff85d2d5756d1
i-fattakhetdinov <fattakhetdinov@mail.ru>
2021-03-15 20:35:13 +0300

    add calculator

---
+++ Calculator/ParsingException.java
@@ -0,0 +1,11 @@
+package Calculator;
+
+public class ParsingException extends Exception {
+    public ParsingException(String message) {
+        super(message);
+    }
+
+    public ParsingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
